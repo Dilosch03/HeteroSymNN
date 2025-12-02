9c310d97618bf0649d426af4a102293d58ce0bbe
Dilosch03 <71047126+Dilosch03@users.noreply.github.com>
2025-12-02 17:35:34 -0600

    updated the version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 
 setup(name="HeteroSymNN",
-      version="0.1.5",
+      version="0.1.6",
       packages=find_packages(),
       python_requires=">=3.9",
       author="Dilosch03",
