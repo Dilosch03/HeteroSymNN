9554fff7b91f69130a12c89c26b810fb602894d5
Dilosch03 <71047126+Dilosch03@users.noreply.github.com>
2025-12-04 13:32:06 -0600

    fix the error where the dependancies does not install

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup,find_packages
 
 setup(name="HeteroSymNN",
-      version="0.1.6",
+      version="0.1.7",
       packages=find_packages(),
       python_requires=">=3.9",
       author="Dilosch03",
-      install_require=["numpy>=2.2.6","sympy>=1.14.0"],
+      install_requires=["numpy~=2.2.0","sympy~=1.14.0"],
       extras_require={
-        "gpu": ["cupy>=13.6.0"]
+        "gpu": ["cupy~=13.6.0"]
         })
